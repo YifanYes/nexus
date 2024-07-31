4098fd9e46da304ac7672f3d32b5ed6af1e57c23
Yifan <yifanyemontpe@gmail.com>
2024-07-31 15:09:03 +0200

    Add test script and character service

---
+++ backend/services/character.service.js
@@ -0,0 +1,39 @@
+const { difficultyRewards, levelRequiremens } = require('../constants')
+
+const completeMission = (character, mission) => {
+  const newExp = difficultyRewards[mission.difficulty]
+  levelUp(character, newExp)
+
+  if (mission.type === 'WORKOUT') {
+    character.strength += 5
+  }
+
+  if (mission.type === 'STUDY') {
+    character.intelligence += 5
+  }
+
+  if (mission.type === 'SOCIAL') {
+    character.charisma += 5
+  }
+
+  if (mission.type === 'HEALTH') {
+    character.health += 5
+  }
+}
+
+const levelUp = (character, newExp) => {
+  const nextLevelRequirement = levelRequiremens[character.level + 1]
+  const totalExp = (character.exp += newExp)
+
+  if (totalExp >= nextLevelRequirement) {
+    const remainder = totalExp - nextLevelRequirement
+    character.level += 1
+    character.exp = remainder
+  }
+
+  character.exp = totalExp
+}
+
+module.exports = {
+  completeMission
+}
---
+++ backend/test.js
@@ -0,0 +1,25 @@
+const { completeMission } = require('./services/character.service')
+
+const character = {
+  health: 100,
+  mana: 50,
+  exp: 0,
+  level: 1,
+  strength: 25,
+  defense: 25,
+  intelligence: 25,
+  charisma: 25,
+  criticalRate: 0,
+  efficiency: 0,
+  trustworthyness: 0
+}
+
+// Complete hard mission
+const mission = {
+  difficulty: 'MYTHIC',
+  type: 'IRON'
+}
+
+completeMission(character, mission)
+
+console.table(character)
